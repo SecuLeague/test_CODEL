cb286b1e8d7158163d3e38a2eb098f597ccc6737
SecuLeague <101701884+SecuLeague@users.noreply.github.com>
2025-02-13 09:53:22 +0100

    Update script3.py

--- Integration CI-CD/script3.py
+++ Integration CI-CD/script3.py
@@ -164,7 +164,7 @@ def get_repository_contents(token, repo_name, local_path):
 
 if __name__ == "__main__":
     # Définir le token GitHub directement dans le script
-    github_token = "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" # Remplacez ceci par votre token GitHub
+    github_token = "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" # Remplacez ceci par votre token GitHub
     local_path = "test_CODEL"  # Chemin local pour cloner le dépôt
 
     if github_token:
